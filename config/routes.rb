94947df8da56c5e7a9b511a0174a84e20f87f1d6
DaniGavli <dngg55@gmail.com>
2021-04-28 22:28:38 -0300

    create posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations'}
-  devise_for:admins, skip:[:registrations], controllers: {sessions: 'admins/sessions'}
+  devise_for :admins, skip:[:registrations], controllers: {sessions: 'admins/sessions'}
  
   namespace :admin do
     root to: "home#index"
     resources :admins
     resources :users, only: :index
-    #cria todas as rotas do admin
+    resources :posts, only: [:index, :show]
+    #cria todas as rotas 
 end
 
 root to: "user/timeline#index"
 
   namespace :user do
+    get 'profile', to: "profile#show"
+    resources :posts, only: :create
 
   end
 end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+    factory :post do
+        body { "Pode ser descrito como uma espécie de saudosismo
+             ou saudade de lugares imaginados como idílicos 
+             ou fantasiosos, onde nunca se esteve antes. 
+             A um nível mais profundo, pode ser entendido como uma busca
+             de natureza filosófica ou religiosa, 
+             que impele o indivíduo como uma locomotiva que não para 
+             até que tenha alcançado sua estação, que pode ser um lugar geográfico, 
+             uma descoberta filosófica ou uma iluminação religiosa." 
+            }
+    end 
+end
